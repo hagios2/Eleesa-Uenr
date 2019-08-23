928fa525a1237d5732fe6a7009247b035185ba57
hagios <hagioswilson@gmail.com>
2019-08-23 17:07:52 -0400

    exporting mysql to postgrel

---
+++ script.lisp
@@ -0,0 +1,3 @@
+LOAD DATABASE
+FROM mysql://admin@localhost/eleesa
+INTO postgres://dporplakgxroxb:065292be31b9db81cedde25d179a26b0cb331e9a8a20feb72ffb37f5edfb1fb8@ec2-174-129-27-158.compute-1.amazonaws.com:5432/d18ms53l8jv4r0
